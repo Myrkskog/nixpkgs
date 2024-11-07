74a28927c848443a6f48530f96f50d9f015b1d4b
magoose <>
2024-11-07 13:13:47 -0500

    URL and Hash issue + GLFW fix

--- pkgs/applications/misc/icesl/default.nix
+++ pkgs/applications/misc/icesl/default.nix
@@ -1,38 +1,61 @@
-{ stdenv, lib, fetchzip, libglut, libXmu, libXi, libX11, libICE, libGLU, libGL, libSM
-, libXext, glibc, lua, luabind, glfw, libgccjit, dialog, makeWrapper
-}:
+{ stdenv, lib, fetchzip, freeglut, libXmu, libXi, libX11, libICE, libGLU, libGL, libSM
+, libXext, gcc-unwrapped, glibc, lua, luabind, glfw, libgccjit, dialog, openssl, makeWrapper }:
 let
-  lpath = lib.makeLibraryPath [ libXmu libXi libX11 libglut libICE libGLU libGL libSM libXext  glibc lua glfw luabind libgccjit ];
+  lpath = lib.makeLibraryPath [ libXmu libXi libX11 freeglut libICE libGLU libGL libSM libXext gcc-unwrapped glibc lua glfw luabind libgccjit openssl ];
+  #makeVersionBeta = beta: "${beta}";
 in
 stdenv.mkDerivation rec {
   pname = "iceSL";
-  version = "2.4.1";
+  version = "2.5.3";
+  _versionType = "version=beta";
 
   src =  if stdenv.hostPlatform.system == "x86_64-linux" then fetchzip {
-    url = "https://icesl.loria.fr/assets/other/download.php?build=${version}&os=amd64";
+    url = "https://icesl.loria.fr/assets/other/download.php?build=${version}&${_versionType}&os=amd64";
     extension = "zip";
-    sha256 = "0rrnkqkhlsjclif5cjbf17qz64vs95ja49xarxjvq54wb4jhbs4l";
+    #sha256= "sha256-db4qj7hg0UnrlCnWAijvMWMgncUWPacnip3jdKU6Vl4=";
+    sha256 = "sha256-wFsyJS3UMw/b4nZTGBg74exVBHTQs6JoLmJs57/D8Co=";
   } else if stdenv.hostPlatform.system == "i686-linux" then fetchzip {
-    url = "https://icesl.loria.fr/assets/other/download.php?build=${version}&os=i386";
+    url = "https://icesl.loria.fr/assets/other/download.php?build=${version}&${_versionType}&os=i386";
     extension = "zip";
     sha256 = "0n2yyxzw0arkc70f0qli4n5chdlh9vc7aqizk4v7825mcglhwlyh";
   } else throw "Unsupported architecture";
 
   nativeBuildInputs = [ makeWrapper ];
-  installPhase = ''
+    installPhase = ''
     cp -r ./ $out
-    rm $out/bin/*.so
     mkdir $out/oldbin
+    mkdir -p $out/icesl-printers/fff/ARES_RED_CUSTOM/{materials,profiles}
+    touch $out/icesl-printers/fff/ARES_RED_CUSTOM/{end.g,features.lua,printer.lua,start.g,startpre.g,swap.g,swappre.g,wait.g}
+    touch $out/icesl-themes/mymonochrome.icss
+    cp $out/bin/liblua.so $out/oldbin/liblua.so
+    cp $out/bin/libluabind.so $out/oldbin/libluabind.so
     mv $out/bin/IceSL-slicer $out/oldbin/IceSL-slicer
     runHook postInstall
   '';
 
+  #added $out/oldbin to rpath to reference liblua.so and libluabind.so
+
   postInstall = ''
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${lpath}" \
+   patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+      --set-rpath "$out/oldbin:${lpath}" \
       $out/oldbin/IceSL-slicer
     makeWrapper $out/oldbin/IceSL-slicer $out/bin/icesl --prefix PATH : ${dialog}/bin
-  '';
+ '';
+
+  #installPhase = ''
+  #  cp -r ./ $out
+  #  rm $out/bin/*.so
+  #  mkdir $out/oldbin
+  #  mv $out/bin/IceSL-slicer $out/oldbin/IceSL-slicer
+  #  runHook postInstall
+  #'';
+
+  #postInstall = ''
+  #  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+  #    --set-rpath "${lpath}" \
+  #    $out/oldbin/IceSL-slicer
+  #  makeWrapper $out/oldbin/IceSL-slicer $out/bin/icesl --prefix PATH : ${dialog}/bin
+  #'';
 
   meta = with lib; {
     description = "GPU-accelerated procedural modeler and slicer for 3D printing";
