c85781663548ae2b4fa4a9cddd0b48ae804ccb17
magoose <>
2024-12-12 13:52:11 -0500

    empty hash for icesl 2.5.4-beta3

--- pkgs/applications/misc/icesl/default.nix
+++ pkgs/applications/misc/icesl/default.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
   src =  if stdenv.hostPlatform.system == "x86_64-linux" then fetchzip {
     url = "https://icesl.loria.fr/assets/other/download.php?build=${version}&${_versionType}&os=amd64";
     extension = "zip";
-    sha256 = "sha256-BtP/lA9EU2H3DvjYny/6842+3nefjZFEz7GP2y+0iJM=";
+    sha256 = "";#"sha256-BtP/lA9EU2H3DvjYny/6842+3nefjZFEz7GP2y+0iJM="; version 2.5.4-beta1
     #sha256 = "sha256-db4qj7hg0UnrlCnWAijvMWMgncUWPacnip3jdKU6Vl4="; #hash for 2.5.3 stable
   } else if stdenv.hostPlatform.system == "i686-linux" then fetchzip {
     url = "https://icesl.loria.fr/assets/other/download.php?build=${version}&${_versionType}&os=i386";
