5614a319b59c3d105be055c6e2730f4e7c1d3f21
magoose <>
2025-01-02 13:21:31 -0500

    struggling with directories

--- pkgs/by-name/sp/sprawl96/package.nix
+++ pkgs/by-name/sp/sprawl96/package.nix
@@ -70,7 +70,7 @@ stdenv.mkDerivation (finalAttrs: {
     mkdir -p "$out/bin"
     mkdir -p "$out/share/quake"
     substituteInPlace Makefile --replace-fail "cp sprawl96.pak /usr/local/games/quake" "cp sprawl96.pak $out/share/quake/sprawl96.pak"
-    substituteInPlace Makefile --replace-fail "/usr/local/games/quake" "$out/bin/sprawl96" "$out/bin/sprawl96/id1"
+    substituteInPlace Makefile --replace-fail "/usr/local/games/quake" "$out/bin/sprawl96"
   '';
 
   enableParallelBuilding = true;
