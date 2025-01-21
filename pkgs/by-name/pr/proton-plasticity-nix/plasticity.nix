5626cc94b9ff5c6721fbf7c4a7dd103a028eca7c
magoose <>
2025-01-21 10:08:55 -0500

    adding plasticity.nix

---
+++ pkgs/by-name/pr/proton-plasticity-nix/plasticity.nix
@@ -0,0 +1,127 @@
+{
+  stdenv,
+  lib,
+  mkWindowsApp,
+  wine,
+  fetchurl,
+  makeDesktopItem,
+  makeDesktopIcon, # This comes with erosanix. It's a handy way to generate desktop icons.
+  copyDesktopItems,
+  copyDesktopIcons, # This comes with erosanix. It's a handy way to generate desktop icons.
+  unzip,
+  system,
+  self,
+  pkgs,
+  setDPI ? null,
+}: let
+  # This registry file sets winebrowser (xdg-open) as the default handler for
+  # text files, instead of Wine's notepad.
+  pname = "proton-plasticity";
+  txtReg = ./txt.reg;
+  # Contains GPU cache, code cache, window state,
+  # and the machine's unique license sig
+  stateDir = "$HOME/.local/share/${pname}/roaming";
+
+  setDPIReg = pkgs.writeText "set-dpi-${toString setDPI}.reg" ''
+    Windows Registry Editor Version 5.00
+    [HKEY_LOCAL_MACHINE\System\CurrentControlSet\Hardware Profiles\Current\Software\Fonts]
+    "LogPixels"=dword:${toString setDPI}
+  '';
+in
+  mkWindowsApp rec {
+    inherit wine pname;
+
+    version = "24.2.6";
+
+    src = builtins.fetchurl {
+      url = "https://github.com/nkallen/plasticity/releases/download/v${version}/Plasticity.msi";
+      sha256 = "";
+    };
+
+    dontUnpack = true;
+    wineArch = "win64";
+
+    enableInstallNotification = true;
+    # This should work, but it doesn't seem to?
+    # More testing required.
+    # fileMap = {
+    #   "${stateDir}" = "drive_c/users/$USER/AppData/Roaming/Plasticity";
+    # };
+    enableMonoBootPrompt = false;
+    fileMapDuringAppInstall = false;
+    persistRegistry = false;
+    # FIXME: runtime layer lacks persistance
+    persistRuntimeLayer = true;
+    # persistRuntimeLayer = false;
+    inputHashMethod = "store-path";
+
+    nativeBuildInputs = [unzip copyDesktopItems copyDesktopIcons];
+
+    winAppInstall =
+      # doubt that this actually helps
+      # winetricks -q corefonts
+      ''
+        # https://askubuntu.com/questions/29552/how-do-i-enable-font-anti-aliasing-in-wine
+        winetricks -q settings fontsmooth=rgb
+        # https://www.advancedinstaller.com/silent-install-exe-msi-applications.html
+        $WINE msiexec /i ${src} /qb!
+        regedit ${txtReg}
+        regedit ${./use-theme-none.reg}
+        regedit ${./wine-breeze-dark.reg}
+        mkdir -p $WINEPREFIX/${stateDir}
+      ''
+      + lib.optionalString (setDPI != null) ''
+        regedit ${setDPIReg}
+      '';
+    winAppPreRun = ''
+      mkdir -p $WINEPREFIX/${stateDir}
+    '';
+
+    winAppRun = ''
+      mkdir -p $WINEPREFIX/${stateDir}
+      wine "$WINEPREFIX/drive_c/Program Files/Plasticity/Plasticity.exe" "$ARGS"
+    '';
+
+    winAppPostRun = "";
+
+    installPhase = ''
+      runHook preInstall
+      ln -s $out/bin/.launcher $out/bin/${pname}
+      runHook postInstall
+    '';
+
+    desktopItems = let
+      mimeTypes = [
+        "application/x-Plasticity"
+        "application/x-plasticity"
+      ];
+    in [
+      (makeDesktopItem {
+        inherit mimeTypes;
+
+        name = pname;
+        exec = pname;
+        icon = pname;
+        desktopName = "Proton Plasticity";
+        genericName = "3D CAD software for Windows-using artists, I guess.";
+        categories = ["Graphics" "Viewer"];
+      })
+    ];
+
+    desktopIcon = makeDesktopIcon {
+      name = "plasticity";
+
+      src = fetchurl {
+        url = "https://www.plasticity.xyz/_next/image?w=256&q=75&url=%2F_next%2Fstatic%2Fmedia%2Ficon_256x256.09a58ec3.png";
+        sha256 = "sha256-OAmFMeIsrMogwTYiney7rNcKkjbSj/64kGb+6zdbRtA=";
+      };
+    };
+
+    meta = with lib; {
+      description = "Plasticity (Proton version)";
+      homepage = "https://www.plasticity.xyz/";
+      license = licenses.unfree;
+      maintainers = with maintainers; [];
+      platforms = ["x86_64-linux"];
+    };
+  }
