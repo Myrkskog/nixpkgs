449087efd29327b8e7960ad6eb4df536553353ea
magoose <>
2025-02-03 09:49:05 -0500

    url...

--- pkgs/applications/misc/blender/default.nix
+++ pkgs/applications/misc/blender/default.nix
@@ -124,7 +124,7 @@ stdenv'.mkDerivation (finalAttrs: {
     })
     (fetchgit {
       name = "assets";
-      url = "https://projects.blender.org/blender/blender-assets/?ref=npr-prototype&submodules=1";
+      url = "https://projects.blender.org/blender/blender-assets/";
       rev = "npr${finalAttrs.version}";
       fetchLFS = true;
       hash = "sha256-6sQ9+upUUvQ9HO/BwYgnIdCghQ+i+ZcEWu4NFJ3Y3A0=";
